05c8a4721f4a4654caf7269df2bfdb713b643deb
jlangley3 <jlangleyiii@aol.com>
2020-06-05 10:32:09 -0400

    challenge1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+
+
+fetch(imgUrl)
+    .then(response => response.json())
+    .then(json => {
+        json.message.forEach(img => {
+            let ul = document.createElement("ul");
+            let li = document.createElement("li");
+            let imgTag = document.createElement("img")
+            imgTag.setAttribute("src", img);
+            li.appendChild(imgTag);
+            ul.appendChild(li);
+            let dogImg = document.querySelector("#dog-image-container");
+            dogImg.appendChild(ul)
+        })
+    })
